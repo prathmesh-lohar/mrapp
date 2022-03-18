51d786175f1f22dd8be7be23c8978fe6f89c89a9
prathmesh-lohar <loharprathmesh123@gmail.com>
2022-03-17 18:18:59 -0700

    image to char

--- mrapp/settings.py
+++ mrapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-31%)bqup#w1p#cjfp+8mx7n5=0*j*+z_5%x2ph%r!=aykhy6&v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- python/ok.py
+++ python/ok.py
@@ -15,9 +15,9 @@
 import requests
 
 # URL = "http://127.0.0.1:8000/api/testing"
-URL = "https://mrappdoct.pythonanywhere.com/api/mrlogin/vaibhav/123"
-
-headers = {'Authorization': 'Token 24a65a31c804b033f8cf32afd11d738d5ff35ba1'}
+# URL = "https://mrappdoct.pythonanywhere.com/api/mrlogin/vaibhav/123"
+URL = "http://44.202.235.223/api/testing"
+headers = {'Authorization': 'Token c9a98edcd98c945259269321c1e73924b5e83998'}
 
 r = requests.get(URL, headers=headers)
 
